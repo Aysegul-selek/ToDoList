448b9a62a3d46565d77d6cf92d8eaa980bcfdf1b
Aysegul-selek <aysegul.selek03@gmail.com>
2024-11-24 23:09:47 +0300

    bugs fixed

--- ToDoAPI.Business/Todo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TodoApi.Core.Entities
-{
-    public class Todo
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public bool IsCompleted { get; set; }
-    }
-}
--- TodoAPI/Controllers/TodoController.cs
+++ TodoAPI/Controllers/TodoController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using TodoApi.Core.Entities;
 
 
 [ApiController]
