fa45a641bc7be070a9eb5a9334d93bb989ec89d9
Aysegul-selek <aysegul.selek03@gmail.com>
2024-12-04 18:19:01 +0300

    Value added to todo entity.

--- ToDo.Entity/Entities/ToDo.cs
+++ ToDo.Entity/Entities/ToDo.cs
@@ -15,6 +15,7 @@ namespace ToDoAPI.Entities.Entities
         public string Title { get; set; }
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now; // Oluşturulma tarihi
         public DateTime DueDate { get; set; }
 
         // İlişkiler
